94b85aa2efa2f9746cffcae2a21d672a80122d1e
Daniel-Taranger <daniel.taranger@bouvet.no>
2019-08-14 15:41:44 +0200

    Initial commit

---
+++ precache-manifest.099091147cb26870a06dbd80a71841ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4de4534c086c9aedf684bb5496653eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c25aa293054108af2933",
+    "url": "/static/css/main.51c95cec.chunk.css"
+  },
+  {
+    "revision": "b266df68505163404583",
+    "url": "/static/js/2.1ec787e3.chunk.js"
+  },
+  {
+    "revision": "c25aa293054108af2933",
+    "url": "/static/js/main.c8fdeff4.chunk.js"
+  },
+  {
+    "revision": "51df72bd850c258c49d1",
+    "url": "/static/js/runtime~main.ae01748d.js"
+  }
+]);
